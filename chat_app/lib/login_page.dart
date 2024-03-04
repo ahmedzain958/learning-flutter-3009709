0c7ca90baf9c3d870e32bde5229e7ac71ecc7fdf
Zain <ahmed.zaineldin@vodafone.com>
2024-03-04 13:33:35 +0200

    padding around the child element

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -7,7 +7,9 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.cyan,),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+      ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -15,23 +17,16 @@ class LoginPage extends StatelessWidget {
         },
       ),
       //TODO: Add the text and image from the design
-      /* body: Text(
-        'Let\'s sign you in!',
-        style: TextStyle(
-            fontSize: 30,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.5),
-      ),*/
+      /* body: */
       body: Column(
         children: [
           Text(
-            'Welcome back! \nYou\'ve been missed',
+            'Let\'s sign you in!',
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.blueGrey,
-            ),
+                fontSize: 30,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.5),
           ),
           Text(
             'Welcome back! \nYou\'ve been missed',
@@ -41,7 +36,17 @@ class LoginPage extends StatelessWidget {
               color: Colors.blueGrey,
             ),
           ),
-          Image.network(src)
+          Image.network(
+            'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+            height: 200,
+          ),
+          Container(
+            height: 150,
+            width: 150,
+            color: Colors.blue,
+            child: FlutterLogo(),
+            padding: EdgeInsets.all(50),
+          )
         ],
       ),
     );

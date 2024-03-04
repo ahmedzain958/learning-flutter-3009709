c0c716a95a56b22fd43e177dc93bd8a3119fefdb
zain <ahmedzain958@gmail.com>
2024-03-04 15:24:04 +0200

    radius

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -5,16 +5,22 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent/*mistake has been here of not showing app bar with color until I discovered
-         that flutter removed the elevation, at this point in video she elevates by 0 to make it transparent*/,
-        elevation: 0/*no need for this now*/,
+        backgroundColor: Colors
+            .transparent /*mistake has been here of not showing app bar with color until I discovered
+         that flutter removed the elevation, at this point in video she elevates by 0 to make it transparent*/
+        ,
+        elevation: 0 /*no need for this now*/,
         title: Text('Hi Pooja'),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
       ),
       body: Container(
         decoration: BoxDecoration(
           color: Colors.indigo,
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(12))
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          ),
         ),
       ),
     );

d5b909dfc0668e7d03c3d58699d4e580123d57e1
zain <ahmedzain958@gmail.com>
2024-03-06 14:58:21 +0200

    Align widget

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -20,79 +22,89 @@ class ChatPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message!',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Image.network(
-                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                  height: 200,
-                )
-              ],
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message!',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  Image.network(
+                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                    height: 200,
+                  )
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
             ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
           ),
-          Container(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message!',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Image.network(
-                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                  height: 200,
-                )
-              ],
+          Align(
+            alignment: Alignment.centerRight /* aligns containers(chat bubbles) to left / right and makes it wrap content not match parent as it was*/,
+            child: Container(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message!',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  Image.network(
+                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                    height: 200,
+                  )
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
             ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
           ),
-          Container(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message!',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Image.network(
-                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                  height: 200,
-                )
-              ],
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message!',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  Image.network(
+                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                    height: 200,
+                  )
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
             ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
           ),
           //TODO: Align both icons to the cornermost edge of the device width.
           Container(
             height: 100,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
                   onPressed: () {},
--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,61 +1,49 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  class LoginPage extends StatelessWidget {
+    const LoginPage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-      ),
-      //TODO: Align Text and image to the vertical & horizontal centre of the device.
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
-
-            //TODO: Align text to centre
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
-            Container(
-              height: 150,
-              width: 150,
-              // child: FlutterLogo(),
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(50),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fitHeight,
-                    image: NetworkImage(
-                        'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
-                  ),
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(24)),
-            )
-          ],
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Button clicked');
+          },
         ),
-      ),
-    );
+        //TODO: Align Text and image to the vertical & horizontal centre of the device.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center/*aligns elements horizontally for the column but vertically for row*/,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.brown,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
+
+              //TODO: Align text to centre
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center/*for text alignment in order not to be like a paragraph text (aligned at left)*/,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
-}

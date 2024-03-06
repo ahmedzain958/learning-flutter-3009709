fea4a7bee7090299a5d0559acd7b4166c3db5abb
zain <ahmedzain958@gmail.com>
2024-03-06 03:46:42 +0200

    center align

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       theme: ThemeData(primarySwatch: Colors.yellow),
-      home: const ChatPage(),
+      home: LoginPage(),
     );
   }
 }

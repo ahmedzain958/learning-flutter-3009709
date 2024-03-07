e21032468c1b8eb47e8c4f611ee46581f38a6a38
Zain <ahmed.zaineldin@vodafone.com>
2024-03-07 16:57:16 +0200

    expanded

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -5,34 +6,6 @@ import 'package:flutter/widgets.dart';
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
-  Widget getChatBubble(alignment, message) {
-    return Align(
-      alignment: alignment,
-      child: Container(
-        padding: EdgeInsets.all(24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '$message',
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            )
-          ],
-        ),
-        margin: EdgeInsets.all(50),
-        decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: Radius.circular(12))),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +27,9 @@ class ChatPage extends StatelessWidget {
           Expanded /*I think expanded takes the remaining space of the already reserved height*/ (
             child: ListView(
               children: [
-                getChatBubble(Alignment.centerLeft, "Hi"),
-                getChatBubble(Alignment.centerRight, "Hi"),
-                getChatBubble(Alignment.centerLeft, "Hi"),
-                //TODO: Align both icons to the cornermost edge of the device width.
+                ChatBubble(alignment: Alignment.centerLeft,message:  "Hi, this is Ahmed Zain"),
+                ChatBubble(alignment: Alignment.centerRight,message:  "Hi, Zain"),
+                ChatBubble(alignment: Alignment.centerLeft,message:  "Hi, this is Ahmed Zain"),
               ],
             ),
           ),
